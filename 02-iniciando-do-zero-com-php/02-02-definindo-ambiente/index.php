e87c4f0ef4e49c733df2fb707c5498629e382274
Lucas Safeno <lugnorio@gmail.com>
2025-06-30 19:10:09 -0300

    #F020 22-02 - Definindo ambiente

---
+++ 02-iniciando-do-zero-com-php/02-02-definindo-ambiente/index.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName(className: "02.02 Defifindo Ambiente");
+
+/*
+ *
+ */
+fullStackPHPClassSession("Debug session", __LINE__);
