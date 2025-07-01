85a03a9584032f6dcea842ebee7e187902bd0a13
Lucas Safeno <lugnorio@gmail.com>
2025-07-01 09:42:20 -0300

    #F038 40-08 - Gestão de diretórios

--- 03-manipulacao-e-tratamento/03-08-gestao-de-diretorios/index.php
+++ 03-manipulacao-e-tratamento/03-08-gestao-de-diretorios/index.php
@@ -6,13 +6,32 @@ fullStackPHPClassName("03.08 - Gestão de diretórios");
  * [ verificar, criar e abrir ] file_exists | is_dir | mkdir  | scandir
  */
 fullStackPHPClassSession("verificar, criar e abrir", __LINE__);
-
+$folder = __DIR__ . "/uploads";
+if (!file_exists($folder) && !is_dir($folder)) {
+  mkdir($folder, 0755);
+} else {
+  var_dump(scandir($folder));
+}
 
 /*
  * [ copiar e renomear ] copy | rename
  */
 fullStackPHPClassSession("copiar e renomear", __LINE__);
+$file = __DIR__ . "/teste.txt";
+var_dump(
+  pathinfo($file),
+  scandir($folder),
+  scandir(__DIR__)
+);
 
+if (!file_exists($file) || !is_file($file)) {
+  fopen($file, 'w');
+} else {
+  // copy($file, $folder . '/' . basename($file));
+  // var_dump(filemtime($file), filemtime(__DIR__ . "/uploads/teste.txt"));
+  // rename(__DIR__ . "/uploads/teste.txt", __DIR__ . "/uploads/" . time() . "." . pathinfo($file)["extension"]);
+  rename($file, __DIR__ . "/uploads/" . time() . "." . pathinfo($file)["extension"]);
+}
 
 /*
  * [ remover e deletar ] unlink | rmdir
