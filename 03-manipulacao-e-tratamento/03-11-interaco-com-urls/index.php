0bf66fb98aff35bd5b50d03154734e6d79fb77ac
Lucas Safeno <lugnorio@gmail.com>
2025-07-01 18:01:09 -0300

    #F041 43-11 - Interação com URLs

--- 03-manipulacao-e-tratamento/03-11-interaco-com-urls/index.php
+++ 03-manipulacao-e-tratamento/03-11-interaco-com-urls/index.php
@@ -6,7 +6,20 @@ fullStackPHPClassName("03.11 - Interação com URLs");
  * [ argumentos ] ?[&[&][&]]
  */
 fullStackPHPClassSession("argumentos", __LINE__);
+echo "<h1><a href='index.php'>Clear</h1>";
 
+echo "<p> <a href='index.php?arg1=true&arg2=true'>Argumentos</a></p>";
+
+$data = [
+  'name' => 'Lucas',
+  'company' => 'AtualTech'
+];
+
+$arguments = http_build_query($data);
+
+echo "<p><a href='index.php?{$arguments}'>Args by Array</a></p>";
+
+var_dump($_GET);
 
 /*
  * [ segurança ] get | strip | filters | validation
