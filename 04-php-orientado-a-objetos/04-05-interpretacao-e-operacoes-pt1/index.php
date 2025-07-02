2fd5b4fc28770447c49d66dcc6118153dc45f3a5
Lucas Safeno <lugnorio@gmail.com>
2025-07-02 19:11:19 -0300

    #F047 49-05 - Interpretação e operações pt1

--- 04-php-orientado-a-objetos/04-05-interpretacao-e-operacoes-pt1/index.php
+++ 04-php-orientado-a-objetos/04-05-interpretacao-e-operacoes-pt1/index.php
@@ -9,14 +9,33 @@ require __DIR__ . "/source/autoload.php";
  * http://php.net/manual/pt_BR/language.oop5.decon.php
  */
 fullStackPHPClassSession("__construct", __LINE__);
-
-
+// $user = new \Source\Interpretation\User();
+$user = new \Source\Interpretation\User(
+  'Lucas',
+  'Tenório',
+  'lucas@email.com'
+);
+var_dump($user);
 /*
  * [ clone ] Executado automaticamente quando um novo objeto é criado a partir do operador clone.
  * http://php.net/manual/pt_BR/language.oop5.cloning.php
  */
 fullStackPHPClassSession("__clone", __LINE__);
+$lucas = $user;
 
+$teste = $lucas;
+$teste->setLastName("Teste");
+$teste->setFirstName("Teste");
+// $teste->setEmail("teste@email.com");
+
+$teste = clone $lucas;
+
+$teste->setFirstName("Teste1");
+$teste->setLastName("Teste1");
+$teste->setEmail("teste1@email.com");
+
+
+var_dump($lucas, $teste);
 
 /*
  * [ destruct ] Executado automaticamente quando o objeto é finalizado
---
+++ 04-php-orientado-a-objetos/04-05-interpretacao-e-operacoes-pt1/source/Interpretation/User.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Source\Interpretation;
+
+class User
+{
+  private $firstName;
+  private $lastName;
+  private $email;
+
+  public function __construct($firstName, $lastName, $email)
+  {
+    $this->firstName  = $firstName;
+    $this->lastName = $lastName;
+    $this->email = $email;
+  }
+
+  /**
+   * Get the value of firstName
+   */
+  public function getFirstName()
+  {
+    return $this->firstName;
+  }
+
+  /**
+   * Get the value of lastName
+   */
+  public function getLastName()
+  {
+    return $this->lastName;
+  }
+
+  /**
+   * Get the value of email
+   */
+  public function getEmail()
+  {
+    return $this->email;
+  }
+
+  /**
+   * Set the value of firstName
+   *
+   * @return  self
+   */
+  public function setFirstName($firstName)
+  {
+    $this->firstName = $firstName;
+
+    return $this;
+  }
+
+  /**
+   * Set the value of lastName
+   *
+   * @return  self
+   */
+  public function setLastName($lastName)
+  {
+    $this->lastName = $lastName;
+
+    return $this;
+  }
+
+  /**
+   * Set the value of email
+   *
+   * @return  self
+   */
+  public function setEmail($email)
+  {
+    $this->email = $email;
+
+    return $this;
+  }
+}
