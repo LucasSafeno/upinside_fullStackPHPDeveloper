0936505226a02fc5a034e1fee87135caca764939
Lucas Safeno <lugnorio@gmail.com>
2025-07-02 17:58:58 -0300

    #F046 48-04 - Carregamento automático

--- 04-php-orientado-a-objetos/04-04-carregamento-automatico/index.php
+++ 04-php-orientado-a-objetos/04-04-carregamento-automatico/index.php
@@ -1,4 +1,7 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("04.04 - Carregamento automático");
 
@@ -6,9 +9,28 @@ fullStackPHPClassName("04.04 - Carregamento automático");
  * [ autoload spl psr-4 ] Carregamento de suas classes com spl_autoload psr-4
  */
 fullStackPHPClassSession("autoload spl psr-4", __LINE__);
+// require __DIR__ . "/source/Loading/User.php";
+// require __DIR__ . "/source/Loading/Address.php";
+// require __DIR__ . "/source/Loading/Company.php";
+
+require __DIR__ . "/source/autoload.php";
+
+$user = new \Source\Loading\User();
+$address = new \Source\Loading\Address();
+$company = new \Source\Loading\Company();
+
+var_dump(
+  $user,
+  $address,
+  $company
+);
 
 
 /*
  * [ autoload composer psr-4 ] https://getcomposer.org/doc/00-intro.md
  */
 fullStackPHPClassSession("autoload composer psr-4", __LINE__);
+require __DIR__ . "/source/vendor/autoload.php";
+
+$email = new PHPMailer;
+var_dump($email);
